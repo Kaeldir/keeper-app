5230956f3b68b11173a79ce04a09bd5eec17e132
Kaeldir <legal.mikael@gmail.com>
2020-09-07 13:23:53 +0200

    Updates

---
+++ precache-manifest.cfa8dda5be4e0825b361dac7212cf5bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd35e81af38276943ba226ad43c000f",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "9b37a4614fd24a13a575",
+    "url": "/keeper-app/static/js/2.2ea54a68.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper-app/static/js/2.2ea54a68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e411ac8dac39336114",
+    "url": "/keeper-app/static/js/main.87ff75d4.chunk.js"
+  },
+  {
+    "revision": "cbb62ef31cd285f39967",
+    "url": "/keeper-app/static/js/runtime-main.f5f4e5a8.js"
+  }
+]);
